098530e093ac38b47aae73281b274904e159c6cc
Ruslan Reznichenko <ruslan.reznschenko9@gmail.com>
2022-08-18 14:11:28 +0300

    last version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+interface EventInform {
+  event: string;
+  
+    day: number,
+    month: string,
+    year: number,
+
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'angular-calendar-';
+
+  displayedColumns: string[] = ['event', 'day', 'month', 'year'];
+
+  eventInform: EventInform[] = [
+    { event: 'work', day: 4, month: 'september', year: 2022 },
+  ]
+
+  allData() {
+    return this.eventInform;
+  }
 }
